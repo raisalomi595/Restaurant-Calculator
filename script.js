c6d42d48b9b60b0019d9c096dd5bd0e528fd3bcb
raisalomi595 <raisalomi595@gmail.com>
2026-08-31 21:35:30 +0545

    feat: Create dynamic menu section

--- script.js
+++ script.js
@@ -21,5 +21,29 @@ const menu = [
     }
 ];
 
+const menuSection = document.createElement('div');
+menuContainer.id = 'menu-container';
+
+const body = document.body;
+body.appendChild(menuContainer);
+
+for (let i= 0; i<menu.length; i++) {
+    const item = menu[i];
+    const itemDiv = document.createElement('div');
+    itemDiv.className = 'menu-item';
+
+    itemDiv.innerHTML = `
+    <h3>${item.name}</h3>
+    <p>Price: Rs. ${item.price}</p>
+    <div>
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+    </div>
+    <p>Item Total: Rs. 0</p>
+    <hr>
+    `;
+    menuContainer.appendChild(itemDiv);
+}
 console.log("Our menu items:", menu);
 console.log("First item:", menu[0].name, "-Rs.", menu[0].price);
