94a40f520a59a8b71fcbf5d6465e7f06e674620c
raisalomi595 <raisalomi595@gmail.com>
2026-08-31 21:02:11 +0545

    feat: Add restaurant menu items

---
+++ script.js
@@ -0,0 +1,25 @@
+const menu = [
+    {
+        name: "Chicken Burger",
+        price: 300
+    },
+    {
+        name: "Veg Burger",
+        price: 270
+    },
+    {
+        name: "Chicken Pizza",
+        price: 500
+    },
+    {
+        name: "Chicken Biryani",
+        price: 400
+    },
+    {
+        name: "French Fries",
+        price: 170
+    }
+];
+
+console.log("Our menu items:", menu);
+console.log("First item:", menu[0].name, "-Rs.", menu[0].price);
